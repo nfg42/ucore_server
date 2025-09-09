0b39a1de53f0d1d99b04c97c6acfd58b1e6aa344
nfg42 <3077570+nfg42@users.noreply.github.com>
2025-09-09 11:08:52 -0500

    Update build.sh

--- ucore/build_files/build.sh
+++ ucore/build_files/build.sh
@@ -11,7 +11,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 #dnf5 install -y systemd-networkd
-dnf5 install -y tmux ansible snapper just powertop yq zip unzip
+dnf5 install -y tmux ansible snapper just powertop yq zip unzip htop
 
 #Fix for ISO install issue
 rm /usr/share/doc/just/README*.md
